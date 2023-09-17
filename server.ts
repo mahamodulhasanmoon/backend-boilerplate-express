54fb838a158e380af5c9dd9c70a7cf299b9d0699
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-09-17 14:51:12 +0600

    done database connection

--- server.ts
+++ server.ts
@@ -1,12 +1,16 @@
 import app from "./src/app/app"
+import dbConnect from "./src/db/dbConnect";
 
-const port =process.env.PORT ||3000
+const port = process.env.PORT ||3000;
+const dbUrl:string = process.env.DB_URI || 'test';
 
-// database connection 
-
-
-
-
-app.listen(port, () => {
-  console.log(`listening on port http://localhost:${port}`)
-})
+(async () => {
+  try {
+    await dbConnect(dbUrl);
+    app.listen(port, () => {
+      console.log(`server is running at ${process.env.SERVER_URI}`)
+    })
+  } catch (error) {
+    console.error('Failed to connect to the database:', error);
+  }
+})();
